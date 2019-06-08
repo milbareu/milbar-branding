a894e2d3ff012927da4ec0b7ec5201e0640b8325
Bartalovics Milán <b.milan@mdsystems.hu>
2019-06-08 08:00:07 +0200

    add custom color scheme, and remove option to change it.

--- src/Init.php
+++ src/Init.php
@@ -5,6 +5,38 @@ namespace Milbar\Branding;
 // Set up plugin class
 class Init
 {
+    /**
+     * Remove Option to change color Scheme from admin
+     */
+    public function admin_color_scheme()
+    {
+        global $_wp_admin_css_colors;
+        $_wp_admin_css_colors = 0;
+    }
+
+    /**
+     * Add milbar color scheme
+     */
+    public function milbar_admin_color_scheme() {
+        wp_admin_css_color( 'milbar', __( 'MilBar' ),
+            MILBAR_BRANDING_PLUGIN_URL . 'assets/css/admin-color.css',
+            array( '#005C8C', '#21678C', '#0085CA', '#ffffff' )
+        );
+    }
+
+    /**
+     * @param $user_id
+     * Set default admin color scheme to milbar when user register.
+     */
+
+    public function set_default_admin_color($user_id) {
+        $args = array(
+            'ID' => $user_id,
+            'admin_color' => 'milbar'
+        );
+        wp_update_user( $args );
+    }
+
     public function __construct()
     {
         add_action('login_enqueue_scripts', [$this, 'login_logo']);
@@ -14,6 +46,9 @@ class Init
         add_action('wp_before_admin_bar_render', [$this, 'menu_custom_logo']);
         add_filter('login_headerurl', [$this, 'login_logo_url']);
         add_filter('login_headertext', [$this, 'login_logo_title']);
+        add_action('admin_head', [$this, 'admin_color_scheme']);
+        add_action('admin_init', [$this, 'milbar_admin_color_scheme']);
+        add_action('user_register', [$this, 'set_default_admin_color']);
     }
 
 
@@ -45,7 +80,6 @@ class Init
     }
 
 
-
     /**
      * Replace login screen logo link
      */
@@ -53,7 +87,6 @@ class Init
     {
         return 'https://milbar.eu';
     }
-
 
 
     // Replace login logo title
@@ -91,13 +124,14 @@ class Init
     {
         ?>
         <style type="text/css">
-            #wpadminbar #wp-admin-bar-my-logo > .ab-item .ab-icon{
+            #wpadminbar #wp-admin-bar-my-logo > .ab-item .ab-icon {
                 height: 20px;
                 width: 20px;
                 margin-right: 0 !important;
                 padding-top: 7px !important;
             }
-            #wpadminbar #wp-admin-bar-my-logo > .ab-item .ab-icon img{
+
+            #wpadminbar #wp-admin-bar-my-logo > .ab-item .ab-icon img {
                 width: 100%;
             }
         </style>
